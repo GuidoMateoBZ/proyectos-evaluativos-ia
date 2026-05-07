37263a2886ef097743d958481ba71a54680b5a2a
GuidoMateoBZ <guidomateobz@gmail.com>
2026-05-07 14:36:42 -0300

    Commit inicial

---
+++ 1er_Proyecto_Evaluativo_IA/nota.py
@@ -0,0 +1,13 @@
+pygad.GA:
+
+parent_selection_type = "tournament"
+
+K_tournament = 3
+
+crossover_type = "two_points"
+
+mutation_type = "random"
+
+mutation_probability = 0.05
+
+stop_criteria = ["reach_500", "saturate_50"]
